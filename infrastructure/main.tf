df1de32936e72f88182326f349fd6fda869a272c
ilirgashii <ilir_gashi_16@hotmail.com>
2026-08-15 20:35:50 +0200

    Add Terraform infrastructure: EC2, security group, RDS

---
+++ infrastructure/main.tf
@@ -0,0 +1,64 @@
+
+provider "aws" {
+	region = "us-east-1"
+}
+
+
+resource "aws_instance" "app_server" {
+  ami		= "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+  tags = {
+    Name = "AWS-Grocery-Server"
+ }
+}
+
+resource "aws_security_group" "app_sg" {
+  name		= "app-server-sg"
+  description	="Allow SSH and HTTP access"
+
+  ingress {
+	description = "SSH"
+	from_port	= 22
+	to_port		= 22
+	protocol	= "tcp"
+	cidr_blocks	= ["0.0.0.0/0"]
+}
+
+  ingress {
+	description	= "HTTP"
+	from_port	= 80
+	to_port		= 80
+	protocol	= "tcp"
+	cidr_blocks	= ["0.0.0.0/0"]
+}
+
+  egress {
+	from_port = 0
+	to_port = 0
+	protocol = "-1"
+	cidr_blocks = ["0.0.0.0/0"]
+}
+
+  tags = {
+	Name = "app-server-sg"
+  }
+}
+
+resource "aws_db_instance" "app_db" {
+ identifier		= "app-grocery-db"
+ engine			= "postgres"
+ engine_version		= "16"
+ instance_class		= "db.t3.micro"
+ allocated_storage	= 20
+ db_name		= "grocerydb"
+ username		= "dbadmin"
+ password		= "admin123"
+ vpc_security_group_ids = [aws_security_group.app_sg.id]
+ skip_final_snapshot	= true
+ publicly_accessible	= true
+
+tags = {
+	name = "AWS-Grocery-DB"
+  }
+}
